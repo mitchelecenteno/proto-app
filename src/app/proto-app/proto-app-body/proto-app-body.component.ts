2fa5baecf87ace0728fc4c4bffa4c0d534f1f9b4
Mitchele Centeno <mitchele.centeno@gmail.com>
2024-04-26 14:48:28 +0800

    wip

--- src/app/proto-app/proto-app-body/proto-app-body.component.ts
+++ src/app/proto-app/proto-app-body/proto-app-body.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./proto-app-body.component.scss']
 })
 export class ProtoAppBodyComponent {
+  products = [
+    {
+      title: 'Discover',
+      description: 'Get information on using these guidelines whenever you are using therefore they have the same style and quality.',
+      image: '../../../assets/product-search.svg'
+    },
+    {
+      title: 'Augmented',
+      description: 'Word salad often arouses curiosity due to its seemingly intelligent and tasteful play on words.',
+      image: '../../../assets/product-smartphone.svg'
+    },
+    {
+      title: 'End Possibilities',
+      description: 'Get information on using these guidelines whenever you are using therefore they have the same style and quality.',
+      image: '../../../assets/product-work.svg'
+    }
 
+  ];
+  alignments = ['right', 'left', 'right']; // Define alignment for each product
 }
